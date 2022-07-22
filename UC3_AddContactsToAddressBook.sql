0e553ca2d88435d504aa676762937300b04804c8
Kisalay <kisalaykisu@gmail.com>
2022-07-22 09:55:05 +0530

    Update UC3_AddContactsToAddressBook.sql

--- UC3_AddContactsToAddressBook.sql
+++ UC3_AddContactsToAddressBook.sql
@@ -1,3 +1,4 @@
+#UC 3 Ability to insert new Contacts to Address Book
 CREATE DATABASE addressbook_service;
 USE addressbook_service;
 CREATE TABLE addressbook (
@@ -17,5 +18,5 @@ VALUES
 ('kisalay','srivastav','random_address','vizag','AP','530013','9346860516','kisalay@gmail.com'),
 ('dipali','sonawane','random_address','vizag','AP','530032','9617726229','dipali@gmail.com'),
 ('subham','verma','random_address','gorakhpur','UP','273001','7894560516','subham@gmail.com'),
-('dinesh','bodepali','random_address','vizag','AP','530009','8332860516','madhukar@gmail.com'),
+('dinesh','bodepali','random_address','vizag','AP','530009','8332860516','kisalay@gmail.com'),
 ('rafi','shaik','random_address','vizag','AP','530011','9982860516','rafi@gmail.com');
