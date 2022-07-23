6b60b777961113ee26c6852a2362ad2bdb14c584
Kisalay <kisalaykisu@gmail.com>
2022-07-23 19:48:42 +0530

    Update UC3_AddContactsToAddressBook.sql

--- UC3_AddContactsToAddressBook.sql
+++ UC3_AddContactsToAddressBook.sql
@@ -1,22 +1,76 @@
 #UC 3 Ability to insert new Contacts to Address Book
-CREATE DATABASE addressbook_service;
-USE addressbook_service;
-CREATE TABLE addressbook (
-	firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL,
-    address varchar(100),
-    city varchar(20),
-    state varchar(30),
-    zip varchar(10),
-    phoneNO varchar(20),
-    email varchar(50)
-);
-DESC addressbook;
 
-INSERT INTO addressbook 
-VALUES 
-('kisalay','srivastav','random_address','vizag','AP','530013','9346860516','kisalay@gmail.com'),
-('dipali','sonawane','random_address','vizag','AP','530032','9617726229','dipali@gmail.com'),
-('subham','verma','random_address','gorakhpur','UP','273001','7894560516','subham@gmail.com'),
-('dinesh','bodepali','random_address','vizag','AP','530009','8332860516','kisalay@gmail.com'),
-('rafi','shaik','random_address','vizag','AP','530011','9982860516','rafi@gmail.com');
+###################################################################################################################################################################
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+7 rows in set (0.00 sec)
+
+##################################################################################################################################################################
+
+mysql> CREATE DATABASE addressbook_service;
+Query OK, 1 row affected (0.01 sec)
+
+###################################################################################################################################################################
+mysql> USE addressbook_service;
+Database changed
+
+###################################################################################################################################################################
+mysql> select database();
++---------------------+
+| database()          |
++---------------------+
+| addressbook_service |
++---------------------+
+1 row in set (0.00 sec)
+
+###################################################################################################################################################################
+mysql> CREATE TABLE addressbook (
+    -> firstName varchar(50) NOT NULL,
+    ->     lastName varchar(50) NOT NULL,
+    ->     address varchar(100),
+    ->     city varchar(20),
+    ->     state varchar(30),
+    ->     zip varchar(10),
+    ->     phoneNO varchar(20),
+    ->     email varchar(50)
+    -> );
+Query OK, 0 rows affected (0.02 sec)
+
+###################################################################################################################################################################
+mysql> DESC addressbook;
++-----------+--------------+------+-----+---------+-------+
+| Field     | Type         | Null | Key | Default | Extra |
++-----------+--------------+------+-----+---------+-------+
+| firstName | varchar(50)  | NO   |     | NULL    |       |
+| lastName  | varchar(50)  | NO   |     | NULL    |       |
+| address   | varchar(100) | YES  |     | NULL    |       |
+| city      | varchar(20)  | YES  |     | NULL    |       |
+| state     | varchar(30)  | YES  |     | NULL    |       |
+| zip       | varchar(10)  | YES  |     | NULL    |       |
+| phoneNO   | varchar(20)  | YES  |     | NULL    |       |
+| email     | varchar(50)  | YES  |     | NULL    |       |
++-----------+--------------+------+-----+---------+-------+
+8 rows in set (0.00 sec)
+
+#################################################################################################################################################################
+mysql> INSERT INTO addressbook
+    -> VALUES
+    -> ('kisalay','srivastav','random_address','vizag','AP','530013','9346860516','kisalay@gmail.com'),
+    -> ('saran','yallanki','random_address','vizag','AP','530032','9617726229','saran@gmail.com'),
+    -> ('subham','verma','random_address','gorakhpur','UP','273001','7894560516','subham@gmail.com'),
+    -> ('dinesh','bodepali','random_address','vizag','AP','530009','8332860516','kisalay@gmail.com'),
+    -> ('rafi','shaik','random_address','vizag','AP','530011','9982860516','rafi@gmail.com');
+Query OK, 5 rows affected (0.00 sec)
+Records: 5  Duplicates: 0  Warnings: 0
+
+########################################################################################################################################################
